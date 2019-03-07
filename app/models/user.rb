198b5d20ab7d3f9d1a2c879bfb874652e5b6b32f
Zartdc <daniel.c.dan@gmail.com>
2019-03-07 10:50:33 -0300

    Relacionamento entre Contato e Usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
                                     format: { with: VALID_EMAIL_REGEX },
                                     uniqueness: { case_sensitive: true }
 
+  has_many :contacts
+
 end
